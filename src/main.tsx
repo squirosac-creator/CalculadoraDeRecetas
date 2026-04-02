1b7d23e2f7ca3af188fae774334b7698ed6a5d14
squirosac-creator <squirosac@ucenfotec.ac.cr>
2026-04-02 16:16:31 -0600

    dist

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+/** @format */
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <NavigationBar />
-  </StrictMode>
+  </StrictMode>,
 );

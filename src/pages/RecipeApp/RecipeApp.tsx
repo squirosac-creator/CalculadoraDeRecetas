a5ba0ba59d1816f32a787d3db3e88fe4de2fa153
squirosac-creator <squirosac@ucenfotec.ac.cr>
2026-04-02 23:14:05 +0000

    fetch

--- src/pages/RecipeApp/RecipeApp.tsx
+++ src/pages/RecipeApp/RecipeApp.tsx
@@ -41,7 +41,7 @@ export default function RecipeApp() {
   const enviarReceta = async () => {
     if (!validarFormulario()) return;
 
-    const response = await fetch("http://127.0.0.1:8000/ajustar-receta", {
+    const response = await fetch("https://calculadoraderecetas-backend.onrender.com/ajustar-receta", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
